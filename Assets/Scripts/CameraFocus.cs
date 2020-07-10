49cb6f3e17427b96c963cd2dfb30c11f4d4be130
CzYoL <zaiyi16121@sina.cn>
2020-07-10 15:33:55 +0800

    不成功便成仁

---
+++ Assets/Scripts/CameraFocus.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class CameraFocus : MonoBehaviour
+{
+    Ray raycast;
+    RaycastHit hit;
+    bool isHit;
+    float hitDistance;
+
+    public Volume volume;
+    DepthOfField depthOfField;
+    public float focusSpeed;
+    public float offset;
+
+    private void Start()
+    {
+        volume.profile.TryGet(out depthOfField);
+    }
+    void Update()
+    {
+        raycast = new Ray(transform.position, transform.forward * 100f);
+        isHit = false;
+        if(Physics.Raycast(raycast, out hit, 100f))
+        {
+            isHit = true;
+            hitDistance = hit.distance + offset;
+            Debug.Log("Hit" + hit.transform.name);
+        }
+        else
+        {
+            if (hitDistance < 100f) hitDistance++;
+        }
+        SetFocus();
+    }
+
+    void SetFocus()
+    {
+        depthOfField.focusDistance.value = Mathf.Lerp(depthOfField.focusDistance.value, hitDistance, Time.deltaTime * focusSpeed);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (isHit)
+        {
+            Gizmos.DrawSphere(hit.point, .1f);
+            Gizmos.DrawRay(transform.position, transform.forward * hitDistance);
+        }
+        else
+        {
+            Gizmos.DrawRay(transform.position, transform.forward * 100f);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController cc;
+    public GameObject fpsCam;
+    public float mouseSensitivity;
+    private float vx, vy,mx,my;
+    public float moveSpeed;
+    private float XRotation;
+    public Animator anim;
+    private Vector3 velocity;
+    
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the player
+        vx = Input.GetAxisRaw("Horizontal");
+        vy = Input.GetAxisRaw("Vertical");
+        Vector3 dir = (transform.forward * vy + transform.right * vx).normalized;
+        Vector3 movement = dir * moveSpeed * Time.deltaTime;
+        if(dir.magnitude > .1f) anim.SetBool("walking", true);
+        else anim.SetBool("walking", false);
+        cc.Move(movement);
+        
+        //add  gravity
+        if (cc.isGrounded) velocity.y = -2f;
+        else velocity.y += Physics.gravity.y * Time.deltaTime;
+        cc.Move(velocity * Time.deltaTime);
+
+
+
+            //move the camera
+        mx = Input.GetAxis("Mouse X");
+        my = Input.GetAxis("Mouse Y");
+        transform.Rotate(Vector3.up * mx * mouseSensitivity * Time.deltaTime);
+        
+        XRotation -= my * mouseSensitivity * Time.deltaTime;
+        XRotation = Mathf.Clamp(XRotation, -89f, 89f);
+        
+        fpsCam.transform.localRotation = Quaternion.Euler(XRotation, 0f, 0f);
+        
+        
+    }
+    
+}
